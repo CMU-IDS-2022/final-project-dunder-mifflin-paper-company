6735dde8f1a3755e40539f7b0c24b662dccc412d
rubenjohn1999 <rubenjohn1999@gmail.com>
2022-04-17 22:37:03 -0400

    Created Model output files for ForeCasting cases

---
+++ apps/model.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import altair as alt
+from vega_datasets import data
+from datetime import datetime, timedelta
+import pydeck as pdk
+import time
+import sklearn.metrics as metrics
+
+
+@st.cache
+def read_files():
+
+    return
+
+
+if __name__ =="__main__":
+    read_files()
